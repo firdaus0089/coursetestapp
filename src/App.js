6d48cb277154ee9110a1a4a10a30071d060f1d16
firdaus0089 <firdaus_0089@hotmail.com>
2023-08-18 10:36:59 +0800

    adding base changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+import NavBar from './NavBar';
+import Home from './Home';
+import Locations from './Locations';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/locations' element={<Locations />} />
+      </Routes>
     </div>
   );
 }
